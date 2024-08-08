cd896002f908ef384f3a8eff62e64e6fe93b2b2e
phamquiduong <phamquiduong@outlook.com>
2024-08-08 19:15:11 +0700

    Create task application

--- src/auth/main.py
+++ src/auth/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 app = FastAPI(
+    title='Task Manager Auth Application',
     docs_url='/auth',
     openapi_url="/auth/openapi.json"
 )
---
+++ src/task/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='Task Manager Application',
+    docs_url='/',
+)
