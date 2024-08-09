66e5c20bb63926fb9802422c362106bbad47f667
Duong Pham Qui <duongpq@nal.vn>
2024-08-09 11:12:32 +0700

    Add user and user session model

--- setup-docker.sh
+++ setup-docker.sh
@@ -8,11 +8,7 @@ else
     echo "alembic.ini already exists, skipping copy"
 fi
 
-# Step 2: Change directory to docker
-cd docker || { echo "Failed to change directory to 'docker'"; exit 1; }
-echo "Changed directory to 'docker'"
-
-# Step 3: Copy .env.example to .env if it doesn't exist
+# Step 2: Copy .env.example to .env if it doesn't exist
 if [ ! -f .env ]; then
     cp .env.example .env
     echo "Copied .env.example to .env"
@@ -20,7 +16,19 @@ else
     echo ".env already exists, skipping copy"
 fi
 
-# Step 4: Create Docker network if it doesn't exist
+# Step 3: Change directory to docker
+cd docker || { echo "Failed to change directory to 'docker'"; exit 1; }
+echo "Changed directory to 'docker'"
+
+# Step 4: Copy .env.example to .env if it doesn't exist
+if [ ! -f .env ]; then
+    cp .env.example .env
+    echo "Copied .env.example to .env"
+else
+    echo ".env already exists, skipping copy"
+fi
+
+# Step 5: Create Docker network if it doesn't exist
 if ! docker network ls | grep -q "task_manager_be_network"; then
     docker network create task_manager_be_network
     echo "Created Docker network 'task_manager_be_network'"
@@ -28,6 +36,6 @@ else
     echo "Docker network 'task_manager_be_network' already exists, skipping"
 fi
 
-# Step 5: Build and run Docker containers
+# Step 6: Build and run Docker containers
 docker-compose up --build -d
 echo "Docker containers are up and running"
