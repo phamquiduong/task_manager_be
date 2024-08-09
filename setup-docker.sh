d4c6cfdb2b43e1cb91114f492ee5cad198a46e65
Duong Pham Qui <duongpq@nal.vn>
2024-08-09 12:12:43 +0700

    Fix postgres cannot start

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
+copy_if_not_exists() {
+    local src=$1
+    local dest=$2
+    if [ ! -f "$dest" ]; then
+        cp "$src" "$dest"
+        echo "Copied $src to $dest"
+    else
+        echo "$dest already exists, skipping copy"
+    fi
+}
+
 # Step 1: Copy alembic.ini.example to alembic.ini if it doesn't exist
-if [ ! -f alembic.ini ]; then
-    cp alembic.ini.example alembic.ini
-    echo "Copied alembic.ini.example to alembic.ini"
-else
-    echo "alembic.ini already exists, skipping copy"
-fi
+copy_if_not_exists "alembic.ini.example" "alembic.ini"
 
 # Step 2: Copy .env.example to .env if it doesn't exist
-if [ ! -f .env ]; then
-    cp .env.example .env
-    echo "Copied .env.example to .env"
-else
-    echo ".env already exists, skipping copy"
-fi
+copy_if_not_exists ".env.example" ".env"
 
 # Step 3: Change directory to docker
 cd docker || { echo "Failed to change directory to 'docker'"; exit 1; }
 echo "Changed directory to 'docker'"
 
-# Step 4: Copy .env.example to .env if it doesn't exist
-if [ ! -f .env ]; then
-    cp .env.example .env
-    echo "Copied .env.example to .env"
-else
-    echo ".env already exists, skipping copy"
-fi
+# Step 4: Down docker-compose
+docker-compose down
+echo "Down docker-compose"
 
-# Step 5: Create Docker network if it doesn't exist
+# Step 5: Copy .env.example to .env if it doesn't exist (inside docker directory)
+copy_if_not_exists ".env.example" ".env"
+
+# Step 6: Create Docker network if it doesn't exist
 if ! docker network ls | grep -q "task_manager_be_network"; then
     docker network create task_manager_be_network
     echo "Created Docker network 'task_manager_be_network'"
@@ -36,6 +36,10 @@ else
     echo "Docker network 'task_manager_be_network' already exists, skipping"
 fi
 
-# Step 6: Build and run Docker containers
+# Step 7: Build and run Docker containers
 docker-compose up --build -d
 echo "Docker containers are up and running"
+
+# Step 8: Run migrate
+docker-compose exec alembic alembic upgrade head
+echo "Run migrate"
