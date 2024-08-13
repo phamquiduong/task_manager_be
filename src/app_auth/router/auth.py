3132e1274428e3f5a0175a2e0ade616753051ccb
Duong Pham Qui <duongpq@nal.vn>
2024-08-13 16:47:51 +0700

    Create API register

--- src/app_auth/router/auth.py
+++ src/app_auth/router/auth.py
@@ -1,8 +1,20 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from database import get_session
 
 auth_router = APIRouter(prefix='/auth')
 
 
 @auth_router.get('/health-check')
 def health_check():
+    return {}
+
+
+@auth_router.post('/register')
+def register(
+    session=Annotated[Session, Depends(get_session)]
+):
     return {}
