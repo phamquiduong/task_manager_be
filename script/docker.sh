a208dc012a4890bce06b04beb9a65db58a104f6f
phamquiduong <phamquiduong@outlook.com>
2024-08-15 00:43:36 +0700

    Update path

--- script/docker.sh
+++ script/docker.sh
@@ -11,26 +11,32 @@ copy_if_not_exists() {
     fi
 }
 
-# Step 1: Copy alembic.ini.example to alembic.ini if it doesn't exist
-cd migration/
+
+# Change directory to the project root
+cd ..
+
+
+# Create project enviroment file if it doesn't exist
+copy_if_not_exists "auth.env.example" "auth.env"
+
+
+# Copy alembic.ini.example to alembic.ini if it doesn't exist
+cd migration || { echo "Failed to change directory to 'migration'"; exit 1; }
 copy_if_not_exists "alembic.ini.example" "alembic.ini"
 cd ..
 
-# Step 2: Copy .env.example to .env if it doesn't exist
-copy_if_not_exists "auth.env.example" "auth.env"
 
-# Step 3: Change directory to docker
+# Change directory to docker
 cd docker || { echo "Failed to change directory to 'docker'"; exit 1; }
-echo "Changed directory to 'docker'"
 
-# Step 4: Down docker-compose
+# Down docker-compose
 docker-compose down
 echo "The Docker containers are down."
 
-# Step 5: Copy .env.example to .env if it doesn't exist (inside docker directory)
+# Create docker-compose enviroment file if it doesn't exist
 copy_if_not_exists ".env.example" ".env"
 
-# Step 6: Create Docker network if it doesn't exist
+# Create Docker network if it doesn't exist
 if ! docker network ls | grep -q "task_manager_be_network"; then
     docker network create task_manager_be_network
     echo "Created Docker network 'task_manager_be_network'"
@@ -38,6 +44,6 @@ else
     echo "Docker network 'task_manager_be_network' already exists, skipping"
 fi
 
-# Step 7: Build and run Docker containers
+# Build and run Docker containers
 docker-compose up --build -d
-echo "Docker containers are up and running"
+echo "Docker containers are up and running. Open http://localhost to view docs"
